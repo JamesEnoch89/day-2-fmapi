01a1f4aeb513a69ca537650b68805d881d9c3a6c
Enoch <jenoch@proforma.com>
2018-03-27 23:41:32 -0400

    finished

--- public/main.js
+++ public/main.js
@@ -1,5 +1,30 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
-}
+const API_KEY = "cb5756bc0dc1ce0f4c0929f0b0954b47"
+const BASE_URL = "http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=" + API_KEY + "&format=json";
 
-document.addEventListener('DOMContentLoaded', main)
+angular
+  .module("mainApp", [])
+  .controller("mainController", [`$scope`, `$http`, ($scope, $http) => {
+    $scope.currentArtist =
+
+      $http({
+        method: "GET",
+        url: BASE_URL
+      }).then(response => {
+        console.log(response);
+        console.log(response.data);
+        $scope.artists = response.data.artists.artist;
+      });
+
+    $scope.loadArtists = (url) => {
+      console.log("loading artists", url)
+      $http({
+        method: "GET",
+        url: url
+      }).then(response => {
+        console.log("loading artists", response)
+        console.log(response.data);
+        $scope.artists = response.data.artists.artist;
+      });
+    }
+
+  }]);
